fixes for mixed precision training

luke/model.py
@@ -9,7 +9,7 @@ from torch import nn
 
 logger = logging.getLogger(__name__)
 
-EPS = 1e-12
+EPS = 1e-7
 
 
 class BertLayerNorm(nn.Module):
@@ -68,7 +68,7 @@ class EntityEmbeddings(nn.Module):
         position_embedding_mask = (position_ids != -1).type_as(position_embeddings).unsqueeze(-1)
         position_embeddings = position_embeddings * position_embedding_mask
         position_embeddings = torch.sum(position_embeddings, dim=-2)
-        position_embeddings = position_embeddings / (position_embedding_mask.sum(dim=-2) + EPS)
+        position_embeddings = position_embeddings / position_embedding_mask.sum(dim=-2).clamp(min=EPS)
 
         token_type_embeddings = self.token_type_embeddings(token_type_ids)
 
@@ -212,7 +212,7 @@ class LukeE2EModel(LukeBaseModel):
                                              [None] * self.config.num_el_hidden_layers)
         el_entity_sequence_output = el_encoder_outputs[0][:, word_seq_size:, :]
         entity_selector_scores = self.entity_selector(el_entity_sequence_output, entity_candidate_ids)
-        entity_selector_scores = entity_selector_scores / self.config.entity_selector_softmax_temp
+        entity_selector_scores = (entity_selector_scores / self.config.entity_selector_softmax_temp).clamp(min=-10000)
         entity_attention_probs = F.softmax(entity_selector_scores, dim=-1)
 
         entity_embedding_output = self.entity_embeddings(entity_candidate_ids, entity_position_ids.unsqueeze(-2),

luke/optimization.py
@@ -3,9 +3,8 @@ from pytorch_transformers.optimization import AdamW
 
 
 class LukeDenseSparseAdam(AdamW):
-    def __init__(self, params, *args, max_grad_norm=1.0, grad_avg_device=None, **kwargs):
+    def __init__(self, params, *args, grad_avg_device=None, **kwargs):
         super(LukeDenseSparseAdam, self).__init__(params, *args, **kwargs)
-        self.max_grad_norm = max_grad_norm
         if grad_avg_device is None:
             self.grad_avg_device = self.param_groups[0]['params'][0].device
         else:
@@ -34,15 +33,11 @@ class LukeDenseSparseAdam(AdamW):
 
                 state['step'] += 1
 
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                exp_avg, exp_avg_sq = state['exp_avg'].to(p.device), state['exp_avg_sq'].to(p.device)
                 beta1, beta2 = group['betas']
-
-                if self.max_grad_norm:
-                    torch.nn.utils.clip_grad_norm_(p, self.max_grad_norm)
 
                 if grad.is_sparse:
                     grad = orig_grad = grad.coalesce()  # the update is non-linear so indices must be unique
-                    grad = grad.to(self.grad_avg_device)
                     grad_values = grad._values()
 
                     old_next_m_values = exp_avg.sparse_mask(grad)._values()
@@ -59,7 +54,6 @@ class LukeDenseSparseAdam(AdamW):
                     del next_m_update_values, next_v_update_values
 
                     update_values = numer / denom
-                    update_values = update_values.to(p.device)
 
                     if group['weight_decay'] > 0.0:
                         update_values += group['weight_decay'] * p.data.sparse_mask(orig_grad)._values()
@@ -68,13 +62,9 @@ class LukeDenseSparseAdam(AdamW):
                     update_with_lr = self._make_sparse(update_with_lr, orig_grad)
 
                 else:
-                    grad = grad.to(self.grad_avg_device)
-
                     exp_avg.mul_(beta1).add_(1.0 - beta1, grad)
                     exp_avg_sq.mul_(beta2).addcmul_(1.0 - beta2, grad, grad)
                     update = exp_avg / (exp_avg_sq.sqrt() + group['eps'])
-
-                    update = update.to(p.device)
 
                     if group['weight_decay'] > 0.0:
                         update += group['weight_decay'] * p.data
@@ -93,13 +83,6 @@ class LukeDenseSparseAdam(AdamW):
         return sparse_tensor.new(indices, values, sparse_tensor.size())
 
     def load_state_dict(self, state_dict):
-        for state in self.state.values():  # for backward compatibility
-            if 'next_m' in state:
-                state['exp_avg'] = state['next_m']
-                del state['next_m']
-                state['exp_avg_sq'] = state['next_v']
-                del state['next_v']
-
         super(LukeDenseSparseAdam, self).load_state_dict(state_dict)
 
         for state in self.state.values():

luke/pretraining/model.py
@@ -34,6 +34,8 @@ class LukePretrainingModel(LukeModel):
     def forward(self, word_ids, word_segment_ids, word_attention_mask, entity_ids, entity_position_ids,
                 entity_segment_ids, entity_attention_mask, masked_entity_labels=None, masked_lm_labels=None,
                 **kwargs):
+        model_dtype = next(self.parameters()).dtype  # for fp16 compatibility
+
         output = super(LukePretrainingModel, self).forward(
             word_ids, word_segment_ids, word_attention_mask, entity_ids, entity_position_ids, entity_segment_ids,
             entity_attention_mask
@@ -41,7 +43,7 @@ class LukePretrainingModel(LukeModel):
         word_sequence_output, entity_sequence_output = output[:2]
 
         loss_fn = CrossEntropyLoss(ignore_index=-1)
-        ret = dict(loss=0.0)
+        ret = dict(loss=word_ids.new_tensor(0.0, dtype=model_dtype))
 
         if masked_entity_labels is not None:
             entity_mask = (masked_entity_labels != -1)
@@ -58,7 +60,7 @@ class LukePretrainingModel(LukeModel):
                 ret['masked_entity_total'] = target_entity_labels.ne(-1).sum()
                 ret['loss'] += ret['masked_entity_loss']
             else:
-                ret['masked_entity_loss'] = word_ids.new_tensor(0.0, dtype=torch.float)
+                ret['masked_entity_loss'] = word_ids.new_tensor(0.0, dtype=model_dtype)
                 ret['masked_entity_correct'] = word_ids.new_tensor(0, dtype=torch.long)
                 ret['masked_entity_total'] = word_ids.new_tensor(0, dtype=torch.long)
 
@@ -77,7 +79,7 @@ class LukePretrainingModel(LukeModel):
                 ret['masked_lm_total'] = masked_lm_labels.ne(-1).sum()
                 ret['loss'] += ret['masked_lm_loss']
             else:
-                ret['masked_lm_loss'] = word_ids.new_tensor(0.0, dtype=torch.float)
+                ret['masked_lm_loss'] = word_ids.new_tensor(0.0, dtype=model_dtype)
                 ret['masked_lm_correct'] = word_ids.new_tensor(0, dtype=torch.long)
                 ret['masked_lm_total'] = word_ids.new_tensor(0, dtype=torch.long)
 
@@ -98,6 +100,8 @@ class LukeE2EPretrainingModel(LukeE2EModel):
     def forward(self, word_ids, word_segment_ids, word_attention_mask, entity_candidate_ids, entity_position_ids,
                 entity_segment_ids, entity_attention_mask, entity_candidate_labels=None, masked_entity_labels=None,
                 masked_lm_labels=None, **kwargs):
+        model_dtype = next(self.parameters()).dtype  # for fp16 compatibility
+
         output = super(LukeE2EPretrainingModel, self).forward(
             word_ids, word_segment_ids, word_attention_mask, entity_candidate_ids, entity_position_ids,
             entity_segment_ids, entity_attention_mask, masked_entity_labels=masked_entity_labels,
@@ -106,7 +110,7 @@ class LukeE2EPretrainingModel(LukeE2EModel):
         word_sequence_output, entity_sequence_output, _, entity_selector_scores = output[:4]
 
         loss_fn = CrossEntropyLoss(ignore_index=-1)
-        ret = dict(loss=word_ids.new_tensor(0.0, dtype=torch.float))
+        ret = dict(loss=word_ids.new_tensor(0.0, dtype=model_dtype))
 
         if masked_entity_labels is not None:
             entity_mask = (masked_entity_labels != -1)
@@ -123,7 +127,7 @@ class LukeE2EPretrainingModel(LukeE2EModel):
                 ret['masked_entity_total'] = target_entity_labels.ne(-1).sum()
                 ret['loss'] += ret['masked_entity_loss']
             else:
-                ret['masked_entity_loss'] = word_ids.new_tensor(0.0, dtype=torch.float)
+                ret['masked_entity_loss'] = word_ids.new_tensor(0.0, dtype=model_dtype)
                 ret['masked_entity_correct'] = word_ids.new_tensor(0, dtype=torch.long)
                 ret['masked_entity_total'] = word_ids.new_tensor(0, dtype=torch.long)
 
@@ -142,8 +146,7 @@ class LukeE2EPretrainingModel(LukeE2EModel):
                 ret['masked_lm_total'] = masked_lm_labels.ne(-1).sum()
                 ret['loss'] += ret['masked_lm_loss']
             else:
-                print(word_ids, word_segment_ids, word_attention_mask)
-                ret['masked_lm_loss'] = word_ids.new_tensor(0.0, dtype=torch.float)
+                ret['masked_lm_loss'] = word_ids.new_tensor(0.0, dtype=model_dtype)
                 ret['masked_lm_correct'] = word_ids.new_tensor(0, dtype=torch.long)
                 ret['masked_lm_total'] = word_ids.new_tensor(0, dtype=torch.long)
 
@@ -157,7 +160,7 @@ class LukeE2EPretrainingModel(LukeE2EModel):
                 ret['entity_selector_total'] = (entity_candidate_labels != -1).sum()
                 ret['loss'] += ret['entity_selector_loss']
             else:
-                ret['entity_selector_loss'] = word_ids.new_tensor(0.0, dtype=torch.float)
+                ret['entity_selector_loss'] = word_ids.new_tensor(0.0, dtype=model_dtype)
                 ret['entity_selector_correct'] = word_ids.new_tensor(0, dtype=torch.long)
                 ret['entity_selector_total'] = word_ids.new_tensor(0, dtype=torch.long)
 

luke/pretraining/train.py
@@ -30,8 +30,9 @@ MASTER_PORT = '29502'
 def run_pretraining(dataset_dir, output_dir, parallel, mode, bert_model_name, batch_size, gradient_accumulation_steps,
                     learning_rate, lr_schedule, warmup_steps, adam_b1, adam_b2, max_grad_norm, masked_lm_prob,
                     masked_entity_prob, whole_word_masking, fix_bert_weights, grad_avg_on_cpu, num_epochs, fp16,
-                    fp16_opt_level, local_rank, log_dir, model_file, optimizer_file, scheduler_file, save_interval_sec,
-                    num_el_hidden_layers=None, entity_selector_softmax_temp=None, global_step=0):
+                    fp16_opt_level, fp16_master_weights, local_rank, log_dir, model_file, optimizer_file,
+                    scheduler_file, master_weights_file, save_interval_sec, num_el_hidden_layers=None,
+                    entity_selector_softmax_temp=None, global_step=0):
     train_args = {}
     for arg in inspect.getfullargspec(run_pretraining).args:
         train_args[arg] = locals()[arg]
@@ -116,14 +117,26 @@ def run_pretraining(dataset_dir, output_dir, parallel, mode, bert_model_name, ba
     if grad_avg_on_cpu:
         grad_avg_device = torch.device('cpu')
     optimizer = LukeDenseSparseAdam(optimizer_parameters, lr=learning_rate, betas=(adam_b1, adam_b2),
-                                    max_grad_norm=max_grad_norm, grad_avg_device=grad_avg_device)
+                                    grad_avg_device=grad_avg_device)
 
     if fp16:
         from apex import amp
-        model, optimizer = amp.initialize(model, optimizer, opt_level=fp16_opt_level)
+        if fp16_opt_level == 'O2':
+            model, optimizer = amp.initialize(model, optimizer, opt_level=fp16_opt_level,
+                                              master_weights=fp16_master_weights)
+        else:
+            model, optimizer = amp.initialize(model, optimizer, opt_level=fp16_opt_level)
 
     if optimizer_file is not None:
         optimizer.load_state_dict(torch.load(optimizer_file, map_location='cpu'))
+        if fp16 and fp16_opt_level == 'O2' and master_weights_file:
+            # https://github.com/NVIDIA/DeepLearningExamples/blob/8546c7a6df2a6c2d338ddadaea210e7332e66af1/PyTorch/LanguageModeling/BERT/run_pretraining.py#L336
+            optimizer._lazy_init_maybe_master_weights()
+            optimizer._amp_stash.lazy_init_called = True
+            optimizer.load_state_dict(torch.load(optimizer_file, map_location='cpu'))
+            master_weights = torch.load(master_weights_file, map_location='cpu')
+            for param, saved_param in zip(amp.master_params(optimizer), master_weights):
+                param.data.copy_(saved_param.data)
 
     if lr_schedule == 'warmup_constant':
         scheduler = WarmupConstantSchedule(optimizer, warmup_steps=warmup_steps)
@@ -168,6 +181,10 @@ def run_pretraining(dataset_dir, output_dir, parallel, mode, bert_model_name, ba
                         model_file=model_file,
                         optimizer_file=optimizer_file,
                         scheduler_file=scheduler_file)
+        if fp16 and fp16_opt_level == 'O2' and fp16_master_weights:
+            master_weights_file = f'master_weights_{suffix}.bin'
+            torch.save(list(amp.master_params(optimizer)), os.path.join(output_dir, master_weights_file))
+            metadata['master_weights_file'] = master_weights_file
         with open(os.path.join(output_dir, f'metadata_{suffix}.json'), 'w') as f:
             json.dump(metadata, f, indent=2, sort_keys=True)
 
@@ -193,7 +210,8 @@ def run_pretraining(dataset_dir, output_dir, parallel, mode, bert_model_name, ba
                 loss = loss / gradient_accumulation_steps
 
             def maybe_no_sync():
-                if hasattr(model, 'no_sync') and num_workers > 1 and accumulation_count + 1 != gradient_accumulation_steps:
+                if hasattr(model, 'no_sync') and num_workers > 1 and\
+                    accumulation_count + 1 != gradient_accumulation_steps:
                     return model.no_sync()
                 else:
                     return contextlib.ExitStack()
@@ -202,8 +220,12 @@ def run_pretraining(dataset_dir, output_dir, parallel, mode, bert_model_name, ba
                 if fp16:
                     with amp.scale_loss(loss, optimizer) as scaled_loss:
                         scaled_loss.backward()
+                    if accumulation_count + 1 == gradient_accumulation_steps and max_grad_norm != 0.0:
+                        torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), max_grad_norm)
                 else:
                     loss.backward()
+                    if accumulation_count + 1 == gradient_accumulation_steps and max_grad_norm != 0.0:
+                        torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
 
         except RuntimeError:
             if prev_error:
@@ -323,12 +345,14 @@ from luke.cli import cli
 @click.option('--grad-avg-on-cpu', is_flag=True)
 @click.option('--num-epochs', default=5)
 @click.option('--fp16', is_flag=True)
-@click.option('--fp16-opt-level', default='O1', type=click.Choice(['O0', 'O1', 'O2', 'O3']))
+@click.option('--fp16-opt-level', default='O1', type=click.Choice(['O1', 'O2']))
+@click.option('--fp16-master-weights/--fp16-no-master-weights', default=True)
 @click.option('--local-rank', '--local_rank', default=-1)
 @click.option('--log-dir', type=click.Path(), default=None)
 @click.option('--model-file', type=click.Path(exists=True), default=None)
 @click.option('--optimizer-file', type=click.Path(exists=True), default=None)
 @click.option('--scheduler-file', type=click.Path(exists=True), default=None)
+@click.option('--master-weights-file', type=click.Path(exists=True), default=None)
 @click.option('--save-interval-sec', default=1800)
 def pretrain(**kwargs):
     run_pretraining(**kwargs)
@@ -350,6 +374,10 @@ def resume_pretraining(output_dir, **kwargs):
     args['model_file'] = os.path.join(output_dir, step_metadata['model_file'])
     args['optimizer_file'] = os.path.join(output_dir, step_metadata['optimizer_file'])
     args['scheduler_file'] = os.path.join(output_dir, step_metadata['scheduler_file'])
+    if 'master_weights_file' in step_metadata:
+        args['master_weights_file'] = os.path.join(output_dir, step_metadata['master_weights_file'])
+    else:
+        args['master_weights_file'] = None
     args['global_step'] = step_metadata['global_step']
 
     for key, value in kwargs.items():