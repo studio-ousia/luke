240963e38ec13de1e29ad3d4de0154431e4997ca
Ikuya Yamada <ikuya@ikuya.net>
2019-09-26 20:16:38 +0900

    fix installation by setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='luke',
     version='0.0.1',
     description='Language understanding with knowledge-based embeddings',
     author='Ikuya Yamada',
-    packages=['luke'],
+    packages=find_packages(),
     include_package_data=True,
+    zip_safe=False,
     entry_points=dict(console_scripts=['luke=luke.cli:cli']),
 )
