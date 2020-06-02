e9fb772ed3c0d69e78dd3d8dfeb0dbc1f3512bb2
linghan <ryou0634@gmail.com>
2020-06-02 14:52:24 +0900

    forgot to add toeknization_xlm_roberta_debugeed.py

---
+++ luke/utils/tokenization_xlm_roberta_debugged.py
@@ -0,0 +1,17 @@
+from transformers import XLMRobertaTokenizer
+
+
+class XLMRobertaTokenizerDebugged(XLMRobertaTokenizer):
+    """
+        The original XLMRobertaTokenizer is broken, so fix that ourselves.
+        (https://github.com/huggingface/transformers/issues/2976)
+    """
+
+    def __init__(self, vocab_file, **kwargs):
+        super().__init__(vocab_file, **kwargs)
+        self.fairseq_tokens_to_ids["<mask>"] = len(self.sp_model) + self.fairseq_offset
+        self.fairseq_ids_to_tokens = {v: k for k, v in self.fairseq_tokens_to_ids.items()}
+
+    @property
+    def vocab_size(self):
+        return len(self.sp_model) + self.fairseq_offset
