f568247b7418a68532b06b0b0aa0151437c7fdff
Ikuya Yamada <ikuya@ikuya.net>
2020-06-16 01:34:45 +0900

    drop the support of sparse gradients in Adam optimizer

--- luke/optimization.py
+++ luke/optimization.py
@@ -3,9 +3,9 @@ import torch
 from transformers.optimization import AdamW
 
 
-class LukeDenseSparseAdam(AdamW):
+class LukeAdamW(AdamW):
     def __init__(self, params, *args, grad_avg_device=None, **kwargs):
-        super(LukeDenseSparseAdam, self).__init__(params, *args, **kwargs)
+        super(LukeAdamW, self).__init__(params, *args, **kwargs)
         if grad_avg_device is None:
             self.grad_avg_device = self.param_groups[0]["params"][0].device
         else:
@@ -21,6 +21,8 @@ class LukeDenseSparseAdam(AdamW):
                 if p.grad is None:
                     continue
                 grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
                 state = self.state[p]
 
@@ -32,62 +34,37 @@ class LukeDenseSparseAdam(AdamW):
                     # Exponential moving average of squared gradient values
                     state["exp_avg_sq"] = torch.zeros_like(p.data, device=self.grad_avg_device)
 
-                state["step"] += 1
-
                 exp_avg, exp_avg_sq = state["exp_avg"].to(p.device), state["exp_avg_sq"].to(p.device)
                 beta1, beta2 = group["betas"]
 
-                if grad.is_sparse:
-                    grad = orig_grad = grad.coalesce()  # the update is non-linear so indices must be unique
-                    grad_values = grad._values()
+                state["step"] += 1
 
-                    old_next_m_values = exp_avg.sparse_mask(grad)._values()
-                    next_m_update_values = grad_values.sub(old_next_m_values).mul_(1 - beta1)
-                    exp_avg.add_(self._make_sparse(next_m_update_values, grad))
-                    old_next_v_values = exp_avg_sq.sparse_mask(grad)._values()
-                    next_v_update_values = grad_values.pow(2).sub_(old_next_v_values).mul_(1 - beta2)
-                    exp_avg_sq.add_(self._make_sparse(next_v_update_values, grad))
+                # Decay the first and second moment running average coefficient
+                # In-place operations to update the averages at the same time
+                exp_avg.mul_(beta1).add_(1.0 - beta1, grad)
+                exp_avg_sq.mul_(beta2).addcmul_(1.0 - beta2, grad, grad)
+                denom = exp_avg_sq.sqrt().add_(group["eps"])
 
-                    # Dense addition again is intended, avoiding another sparse_mask
-                    numer = next_m_update_values.add_(old_next_m_values)
-                    next_v_update_values.add_(old_next_v_values)
-                    denom = next_v_update_values.sqrt_().add_(group["eps"])
-                    del next_m_update_values, next_v_update_values
-
-                    update_values = numer / denom
-
-                    if group["weight_decay"] > 0.0:
-                        update_values += group["weight_decay"] * p.data.sparse_mask(orig_grad)._values()
-
-                    update_with_lr = -group["lr"] * update_values
-                    update_with_lr = self._make_sparse(update_with_lr, orig_grad)
-
-                else:
-                    exp_avg.mul_(beta1).add_(1.0 - beta1, grad)
-                    exp_avg_sq.mul_(beta2).addcmul_(1.0 - beta2, grad, grad)
-                    update = exp_avg / (exp_avg_sq.sqrt() + group["eps"])
-
-                    if group["weight_decay"] > 0.0:
-                        update += group["weight_decay"] * p.data
-
-                    update_with_lr = -group["lr"] * update
+                p.data.addcdiv_(-group["lr"], exp_avg, denom)
 
                 state["exp_avg"] = exp_avg.to(self.grad_avg_device)
                 state["exp_avg_sq"] = exp_avg_sq.to(self.grad_avg_device)
 
-                p.data.add_(update_with_lr)
+                # Just adding the square of the weights to the loss function is *not*
+                # the correct way of using L2 regularization/weight decay with Adam,
+                # since that will interact with the m and v parameters in strange ways.
+                #
+                # Instead we want to decay the weights in a manner that doesn't interact
+                # with the m/v parameters. This is equivalent to adding the square
+                # of the weights to the loss with plain (non-momentum) SGD.
+                # Add weight decay at the end (fixed version)
+                if group["weight_decay"] > 0.0:
+                    p.data.add_(-group["lr"] * group["weight_decay"], p.data)
 
         return loss
 
-    @staticmethod
-    def _make_sparse(values: torch.Tensor, sparse_tensor: torch.Tensor):
-        indices = sparse_tensor._indices()
-        if indices.dim() == 0 or values.dim() == 0:
-            return sparse_tensor.new().resize_as_(sparse_tensor)
-        return sparse_tensor.new(indices, values, sparse_tensor.size())
-
     def load_state_dict(self, state_dict: Dict[str, torch.Tensor]):
-        super(LukeDenseSparseAdam, self).load_state_dict(state_dict)
+        super(LukeAdamW, self).load_state_dict(state_dict)
 
         for state in self.state.values():
             if "exp_avg" in state:
