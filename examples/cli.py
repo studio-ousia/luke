2ffc4ff6d4110119bd0015922c018bb78145c77d
Ikuya Yamada <ikuya@ikuya.net>
2020-06-28 18:09:25 +0900

    add CometLogger

--- examples/cli.py
+++ examples/cli.py
@@ -5,11 +5,11 @@ import random
 import string
 import subprocess
 import sys
+from argparse import Namespace
 
 logging.getLogger("transformers").setLevel(logging.WARNING)
 
 import click
-import comet_ml
 import torch
 from transformers import AutoTokenizer
 
@@ -17,6 +17,7 @@ from luke.model import LukeConfig
 from luke.utils.entity_vocab import EntityVocab
 from luke.utils.model_utils import ENTITY_VOCAB_FILE, METADATA_FILE
 
+from .utils.experiment_logger import commet_logger_args, CometLogger, NullLogger
 from .utils.mention_db import MentionDB
 
 LOG_FORMAT = "[%(asctime)s] [%(levelname)s] %(message)s (%(funcName)s@%(filename)s:%(lineno)s)"
@@ -35,49 +36,34 @@ logger = logging.getLogger(__name__)
 
 @click.group()
 @click.option(
-    "--output-dir",
-    default="exp_" + "".join(random.choice(string.ascii_letters) for m in range(8)),
-    type=click.Path(),
-    required=True,
+    "--output-dir", default="exp_" + "".join(random.choice(string.ascii_letters) for m in range(8)), type=click.Path()
 )
 @click.option("--verbose", is_flag=True)
 @click.option("--num-gpus", default=1)
+@click.option("--experiment-logger", "--logger", type=click.Choice(["comet"]))
 @click.option("--master-port", default=29500)
 @click.option("--local-rank", "--local_rank", default=-1)
 @click.option("--model-dir", type=click.Path(exists=True))
 @click.option("--weights-file", type=click.Path(exists=True))
 @click.option("--mention-db-file", type=click.Path(exists=True))
-@click.option("--comet-project")
-@click.option("--comet-offline", is_flag=True)
-@click.option("--comet-offline-dir", type=click.Path(), default="comet_experiments")
+@commet_logger_args
 @click.pass_context
-def cli(
-    ctx,
-    num_gpus,
-    master_port,
-    output_dir,
-    verbose,
-    local_rank,
-    model_dir,
-    weights_file,
-    mention_db_file,
-    comet_project,
-    comet_offline,
-    comet_offline_dir,
-):
-    if local_rank == -1 and num_gpus > 1:
+def cli(ctx, **kwargs):
+    args = Namespace(**kwargs)
+
+    if args.local_rank == -1 and args.num_gpus > 1:
         current_env = os.environ.copy()
         current_env["MASTER_ADDR"] = "127.0.0.1"
-        current_env["MASTER_PORT"] = str(master_port)
-        current_env["WORLD_SIZE"] = str(num_gpus)
+        current_env["MASTER_PORT"] = str(args.master_port)
+        current_env["WORLD_SIZE"] = str(args.num_gpus)
 
         processes = []
 
-        for local_rank in range(0, num_gpus):
-            current_env["RANK"] = str(local_rank)
-            current_env["LOCAL_RANK"] = str(local_rank)
+        for args.local_rank in range(0, args.num_gpus):
+            current_env["RANK"] = str(args.local_rank)
+            current_env["LOCAL_RANK"] = str(args.local_rank)
 
-            cmd = [sys.executable, "-u", "-m", "examples.cli", "--local-rank={}".format(local_rank)]
+            cmd = [sys.executable, "-u", "-m", "examples.cli", "--local-rank={}".format(args.local_rank)]
             cmd.extend(sys.argv[1:])
 
             process = subprocess.Popen(cmd, env=current_env)
@@ -91,63 +77,41 @@ def cli(
         sys.exit(0)
 
     else:
-        if local_rank not in (-1, 0):
+        if args.local_rank not in (-1, 0):
             logging.basicConfig(format=LOG_FORMAT, level=logging.WARNING)
-        elif verbose:
+        elif args.verbose:
             logging.basicConfig(format=LOG_FORMAT, level=logging.DEBUG)
         else:
             logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
 
-        if not os.path.exists(output_dir) and local_rank in [-1, 0]:
-            os.makedirs(output_dir)
-            logger.info("Output dir: %s", output_dir)
+        if not os.path.exists(args.output_dir) and args.local_rank in [-1, 0]:
+            os.makedirs(args.output_dir)
+            logger.info("Output dir: %s", args.output_dir)
 
         # NOTE: ctx.obj is documented here: http://click.palletsprojects.com/en/7.x/api/#click.Context.obj
-        ctx.obj = dict(local_rank=local_rank, output_dir=output_dir)
+        ctx.obj = dict(local_rank=args.local_rank, output_dir=args.output_dir)
 
-        if local_rank == -1:
+        if args.local_rank == -1:
             ctx.obj["device"] = torch.device("cuda")
         else:
-            torch.cuda.set_device(local_rank)
-            ctx.obj["device"] = torch.device("cuda", local_rank)
+            torch.cuda.set_device(args.local_rank)
+            ctx.obj["device"] = torch.device("cuda", args.local_rank)
             torch.distributed.init_process_group(backend="nccl")
 
-        if "experiment" not in ctx.obj and ctx.obj["local_rank"] in (-1, 0) and comet_project:
-            if comet_offline:
-                ctx.obj["experiment"] = comet_ml.OfflineExperiment(
-                    project_name=comet_project,
-                    offline_directory=comet_offline_dir,
-                    auto_metric_logging=False,
-                    auto_output_logging=None,
-                    log_code=False,
-                    log_graph=False,
-                    log_env_host=False,
-                    log_env_gpu=False,
-                    log_env_cpu=False,
-                )
-            else:
-                ctx.obj["experiment"] = comet_ml.Experiment(
-                    project_name=comet_project,
-                    auto_metric_logging=False,
-                    auto_output_logging=None,
-                    log_code=False,
-                    log_graph=False,
-                    log_env_host=False,
-                    log_env_gpu=False,
-                    log_env_cpu=False,
-                )
-        else:
-            ctx.obj["experiment"] = comet_ml.OfflineExperiment(offline_directory=output_dir, disabled=True)
+        experiment_logger = NullLogger()
 
-        local_dict = locals()
-        ctx.obj["experiment"].log_parameters({p.name: local_dict[p.name] for p in cli.params})
+        if args.local_rank in (-1, 0) and args.experiment_logger == "comet":
+            experiment_logger = CometLogger(args)
 
-        if model_dir or weights_file:
-            if not model_dir:
-                model_dir = os.path.dirname(weights_file)
-            ctx.obj["model_dir"] = model_dir
+        experiment_logger.log_parameters({p.name: getattr(args, p.name) for p in cli.params})
+        ctx.obj["experiment"] = experiment_logger
 
-            json_file = os.path.join(model_dir, METADATA_FILE)
+        if args.model_dir or args.weights_file:
+            if not args.model_dir:
+                args.model_dir = os.path.dirname(args.weights_file)
+            ctx.obj["model_dir"] = args.model_dir
+
+            json_file = os.path.join(args.model_dir, METADATA_FILE)
             with open(json_file) as f:
                 model_data = json.load(f)
 
@@ -156,18 +120,18 @@ def cli(
                 model_data["model_config"]["entity_emb_size"] = model_data["model_config"]["hidden_size"]
 
             ctx.obj["tokenizer"] = AutoTokenizer.from_pretrained(model_data["model_config"]["bert_model_name"])
-            ctx.obj["entity_vocab"] = EntityVocab(os.path.join(model_dir, ENTITY_VOCAB_FILE))
+            ctx.obj["entity_vocab"] = EntityVocab(os.path.join(args.model_dir, ENTITY_VOCAB_FILE))
             ctx.obj["bert_model_name"] = model_data["model_config"]["bert_model_name"]
             ctx.obj["model_config"] = LukeConfig(**model_data["model_config"])
             ctx.obj["max_mention_length"] = model_data["max_mention_length"]
 
-        if weights_file:
-            ctx.obj["weights_file"] = weights_file
-            ctx.obj["model_weights"] = torch.load(weights_file, map_location="cpu")
-            ctx.obj["experiment"].log_parameter("weights_file_name", os.path.basename(weights_file))
+        if args.weights_file:
+            ctx.obj["weights_file"] = args.weights_file
+            ctx.obj["model_weights"] = torch.load(args.weights_file, map_location="cpu")
+            ctx.obj["experiment"].log_parameter("weights_file_name", os.path.basename(args.weights_file))
 
-        if mention_db_file:
-            ctx.obj["mention_db"] = MentionDB(mention_db_file)
+        if args.mention_db_file:
+            ctx.obj["mention_db"] = MentionDB(args.mention_db_file)
         else:
             ctx.obj["mention_db"] = None
 
---
+++ examples/utils/experiment_logger.py
@@ -0,0 +1,65 @@
+import click
+import comet_ml
+import functools
+
+
+def commet_logger_args(func):
+    @functools.wraps(func)
+    @click.option("--comet-project-name")
+    @click.option("--comet-offline", is_flag=True)
+    @click.option("--comet-offline-dir", type=click.Path(exists=True), default="comet_experiments")
+    @click.option("--comet-auto-metric-logging", is_flag=True)
+    @click.option("--comet-auto-output-logging", is_flag=True)
+    @click.option("--comet-log-code", is_flag=True)
+    @click.option("--comet-log-env-cpu", is_flag=True)
+    @click.option("--comet-log-env-gpu", is_flag=True)
+    @click.option("--comet-log-env-host", is_flag=True)
+    @click.option("--comet-log-graph", is_flag=True)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+class NullLogger:
+    def log_metric(self, *args, **kwargs):
+        pass
+
+    def log_metrics(self, *args, **kwargs):
+        pass
+
+    def log_parameter(self, *args, **kwargs):
+        pass
+
+    def log_parameters(self, *args, **kwargs):
+        pass
+
+
+class CometLogger:
+    def __init__(self, args):
+        comet_args = dict(
+            project_name=args.comet_project_name,
+            auto_metric_logging=args.comet_auto_metric_logging,
+            auto_output_logging=args.comet_auto_output_logging,
+            log_code=args.comet_log_code,
+            log_env_cpu=args.comet_log_env_cpu,
+            log_env_gpu=args.comet_log_env_gpu,
+            log_env_host=args.comet_log_env_host,
+            log_graph=args.comet_log_graph,
+        )
+        if args.comet_offline:
+            self.logger = comet_ml.OfflineExperiment(offline_directory=args.comet_offline_dir, **comet_args)
+        else:
+            self.logger = comet_ml.Experiment(**comet_args)
+
+    def log_metric(self, *args, **kwargs):
+        self.logger.log_metric(*args, **kwargs)
+
+    def log_metrics(self, *args, **kwargs):
+        self.logger.log_metrics(*args, **kwargs)
+
+    def log_parameter(self, *args, **kwargs):
+        self.logger.log_parameter(*args, **kwargs)
+
+    def log_parameters(self, *args, **kwargs):
+        self.logger.log_parameters(*args, **kwargs)
