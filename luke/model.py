228b018d494a95b0f708c4ed76e19babb3779b92
Ikuya Yamada <ikuya@ikuya.net>
2020-06-28 15:35:09 +0900

    add LukeEntityAwareAttentionModel

--- luke/model.py
+++ luke/model.py
@@ -1,10 +1,20 @@
+import logging
+import math
 from typing import Dict
 
-import logging
-
 import torch
+import torch.nn.functional as F
 from torch import nn
-from transformers.modeling_bert import BertConfig, BertEmbeddings, BertEncoder, BertLayerNorm, BertPooler
+from transformers.modeling_bert import (
+    BertConfig,
+    BertEmbeddings,
+    BertEncoder,
+    BertIntermediate,
+    BertLayerNorm,
+    BertOutput,
+    BertPooler,
+    BertSelfOutput,
+)
 from transformers.modeling_roberta import RobertaEmbeddings
 
 logger = logging.getLogger(__name__)
@@ -172,3 +182,157 @@ class LukeModel(nn.Module):
         extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
 
         return extended_attention_mask
+
+
+class LukeEntityAwareAttentionModel(LukeModel):
+    def __init__(self, config):
+        super(LukeEntityAwareAttentionModel, self).__init__(config)
+        self.config = config
+        self.encoder = EntityAwareEncoder(config)
+
+    def forward(
+        self,
+        word_ids,
+        word_segment_ids,
+        word_attention_mask,
+        entity_ids,
+        entity_position_ids,
+        entity_segment_ids,
+        entity_attention_mask,
+    ):
+        word_embeddings = self.embeddings(word_ids, word_segment_ids)
+        entity_embeddings = self.entity_embeddings(entity_ids, entity_position_ids, entity_segment_ids)
+        attention_mask = self._compute_extended_attention_mask(word_attention_mask, entity_attention_mask)
+
+        return self.encoder(word_embeddings, entity_embeddings, attention_mask)
+
+    def load_state_dict(self, state_dict, *args, **kwargs):
+        new_state_dict = state_dict.copy()
+
+        for num in range(self.config.num_hidden_layers):
+            for attr_name in ("weight", "bias"):
+                if f"encoder.layer.{num}.attention.self.w2e_query.{attr_name}" not in state_dict:
+                    new_state_dict[f"encoder.layer.{num}.attention.self.w2e_query.{attr_name}"] = state_dict[
+                        f"encoder.layer.{num}.attention.self.query.{attr_name}"
+                    ]
+                if f"encoder.layer.{num}.attention.self.e2w_query.{attr_name}" not in state_dict:
+                    new_state_dict[f"encoder.layer.{num}.attention.self.e2w_query.{attr_name}"] = state_dict[
+                        f"encoder.layer.{num}.attention.self.query.{attr_name}"
+                    ]
+                if f"encoder.layer.{num}.attention.self.e2e_query.{attr_name}" not in state_dict:
+                    new_state_dict[f"encoder.layer.{num}.attention.self.e2e_query.{attr_name}"] = state_dict[
+                        f"encoder.layer.{num}.attention.self.query.{attr_name}"
+                    ]
+
+        kwargs["strict"] = False
+        super(LukeEntityAwareAttentionModel, self).load_state_dict(new_state_dict, *args, **kwargs)
+
+
+class EntityAwareSelfAttention(nn.Module):
+    def __init__(self, config):
+        super(EntityAwareSelfAttention, self).__init__()
+
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.key = nn.Linear(config.hidden_size, self.all_head_size)
+        self.value = nn.Linear(config.hidden_size, self.all_head_size)
+
+        self.w2e_query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.e2w_query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.e2e_query = nn.Linear(config.hidden_size, self.all_head_size)
+
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        return x.view(*new_x_shape).permute(0, 2, 1, 3)
+
+    def forward(self, word_hidden_states, entity_hidden_states, attention_mask):
+        word_size = word_hidden_states.size(1)
+
+        w2w_query_layer = self.transpose_for_scores(self.query(word_hidden_states))
+        w2e_query_layer = self.transpose_for_scores(self.w2e_query(word_hidden_states))
+        e2w_query_layer = self.transpose_for_scores(self.e2w_query(entity_hidden_states))
+        e2e_query_layer = self.transpose_for_scores(self.e2e_query(entity_hidden_states))
+
+        key_layer = self.transpose_for_scores(self.key(torch.cat([word_hidden_states, entity_hidden_states], dim=1)))
+
+        w2w_key_layer = key_layer[:, :, :word_size, :]
+        e2w_key_layer = key_layer[:, :, :word_size, :]
+        w2e_key_layer = key_layer[:, :, word_size:, :]
+        e2e_key_layer = key_layer[:, :, word_size:, :]
+
+        w2w_attention_scores = torch.matmul(w2w_query_layer, w2w_key_layer.transpose(-1, -2))
+        w2e_attention_scores = torch.matmul(w2e_query_layer, w2e_key_layer.transpose(-1, -2))
+        e2w_attention_scores = torch.matmul(e2w_query_layer, e2w_key_layer.transpose(-1, -2))
+        e2e_attention_scores = torch.matmul(e2e_query_layer, e2e_key_layer.transpose(-1, -2))
+
+        word_attention_scores = torch.cat([w2w_attention_scores, w2e_attention_scores], dim=3)
+        entity_attention_scores = torch.cat([e2w_attention_scores, e2e_attention_scores], dim=3)
+        attention_scores = torch.cat([word_attention_scores, entity_attention_scores], dim=2)
+
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_scores = attention_scores + attention_mask
+
+        attention_probs = F.softmax(attention_scores, dim=-1)
+        attention_probs = self.dropout(attention_probs)
+
+        value_layer = self.transpose_for_scores(
+            self.value(torch.cat([word_hidden_states, entity_hidden_states], dim=1))
+        )
+        context_layer = torch.matmul(attention_probs, value_layer)
+
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+
+        return context_layer[:, :word_size, :], context_layer[:, word_size:, :]
+
+
+class EntityAwareAttention(nn.Module):
+    def __init__(self, config):
+        super(EntityAwareAttention, self).__init__()
+        self.self = EntityAwareSelfAttention(config)
+        self.output = BertSelfOutput(config)
+
+    def forward(self, word_hidden_states, entity_hidden_states, attention_mask):
+        word_self_output, entity_self_output = self.self(word_hidden_states, entity_hidden_states, attention_mask)
+        hidden_states = torch.cat([word_hidden_states, entity_hidden_states], dim=1)
+        self_output = torch.cat([word_self_output, entity_self_output], dim=1)
+        output = self.output(self_output, hidden_states)
+        return output[:, : word_hidden_states.size(1), :], output[:, word_hidden_states.size(1) :, :]
+
+
+class EntityAwareLayer(nn.Module):
+    def __init__(self, config):
+        super(EntityAwareLayer, self).__init__()
+
+        self.attention = EntityAwareAttention(config)
+        self.intermediate = BertIntermediate(config)
+        self.output = BertOutput(config)
+
+    def forward(self, word_hidden_states, entity_hidden_states, attention_mask):
+        word_attention_output, entity_attention_output = self.attention(
+            word_hidden_states, entity_hidden_states, attention_mask
+        )
+        attention_output = torch.cat([word_attention_output, entity_attention_output], dim=1)
+        intermediate_output = self.intermediate(attention_output)
+        layer_output = self.output(intermediate_output, attention_output)
+
+        return layer_output[:, : word_hidden_states.size(1), :], layer_output[:, word_hidden_states.size(1) :, :]
+
+
+class EntityAwareEncoder(nn.Module):
+    def __init__(self, config):
+        super(EntityAwareEncoder, self).__init__()
+        self.layer = nn.ModuleList([EntityAwareLayer(config) for _ in range(config.num_hidden_layers)])
+
+    def forward(self, word_hidden_states, entity_hidden_states, attention_mask):
+        for layer_module in self.layer:
+            word_hidden_states, entity_hidden_states = layer_module(
+                word_hidden_states, entity_hidden_states, attention_mask
+            )
+        return word_hidden_states, entity_hidden_states
