fb4f2b5a43c0114ab6a00c68dc773933712cb6d6
Ryou0634 <ryou0634@gmail.com>
2022-06-03 17:33:24 +0900

    update

--- split_entity_vocab.py
+++ split_entity_vocab.py
@@ -4,25 +4,27 @@ from pathlib import Path
 
 @click.command()
 @click.argument("entity-vocab-path", type=click.Path(exists=True))
-@click.argument("output-directory", type=click.Path(exists=True))
+@click.argument("output-directory", type=click.Path())
 def split_entity_vocab(entity_vocab_path: str, output_directory: str):
     print(f"Read vocab from {entity_vocab_path}....")
     with open(entity_vocab_path, "r") as f:
         lines = f.read().strip().split("\n")
 
+    Path(output_directory).mkdir(parents=True, exist_ok=True)
+
     filename_stem = Path(entity_vocab_path).stem
     suffix = Path(entity_vocab_path).suffix
-    for start, end in [
-        [0, 100],
-        [100, 1000],
-        [1000, 10000],
-        [10000, 100000],
-        [100000, 200000],
-        [200000, 300000],
-        [300000, 400000],
-        [400000, 500000],
+    for start, end, start_str, end_str in [
+        [0, 100, "0", "100"],
+        [100, 1000, "100", "1k"],
+        [1000, 10000, "1k", "10k"],
+        [10000, 100000, "10k", "100k"],
+        [100000, 200000, "100k", "200k"],
+        [200000, 300000, "200k", "300k"],
+        [300000, 400000, "300k", "400k"],
+        [400000, 500000, "400k", "500k"],
     ]:
-        output_file_path = Path(output_directory) / f"{filename_stem}-{start}_{end}{suffix}"
+        output_file_path = Path(output_directory) / f"{filename_stem}-{start_str}_{end_str}{suffix}"
 
         print(f"Write vocab to {output_file_path}....")
         with open(output_file_path, "w") as f:
