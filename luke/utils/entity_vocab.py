24f213920dcb4a078ae64b544b8cde315c42ff81
Ikuya Yamada <ikuya@ikuya.net>
2022-07-06 14:18:02 +0900

    include all entity titles in the whitelist into the vocabulary

--- luke/utils/entity_vocab.py
+++ luke/utils/entity_vocab.py
@@ -200,8 +200,7 @@ class EntityVocab:
         title_dict[MASK_TOKEN] = 0
 
         for title in white_list:
-            if counter[title] != 0:
-                title_dict[title] = counter[title]
+            title_dict[title] = counter[title]
 
         if not white_list_only:
             valid_titles = frozenset(dump_db.titles())
